15403e1beb9987f8ee8a749df3cff19e42178b48
Anton Zhary <anton.zhary@gmail.com>
2026-02-14 07:01:48 +0300

    feat(ideas): implement modal component
    - Create reusable modal blade component
    - Replace inline modal in ideas index with x-modal component

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -50,24 +50,8 @@
             </div>
         </div>
 
-        <div
-            x-data="{show: false, name: 'create-idea'}"
-            x-show="show"
-            @open-modal.window="if($event.detail === name) show = true;"
-            @keydown.escape.window="show = false"
-            x-transition:enter="ease-out duration-200"
-            x-transition:enter-start="opacity-0 -translate-y-4 -translate-x-4"
-            x-transition:enter-end="opacity-100"
-            x-transition:leave="ease-in duration-150"
-            x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0 -translate-y-4 -translate-x-4"
-            class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-xs"
-            x-cloak
-            role="dialog">
-            <x-card @click.away="show = false">
-                <p>I am a modal</p>
-            </x-card>
-        </div>
-
+<x-modal name="create-idea" title="New Idea">
+    <p>Slot</p>
+</x-modal>
     </div>
 </x-layout>
